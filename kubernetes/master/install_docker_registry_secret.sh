5bdd107f469cd9066f71514abd5b87412139fc13
zhaoxiang <13823394@qq.com>
2018-05-21 23:21:13 +0800

    。

--- kubernetes/master/install_docker_registry_secret.sh
+++ kubernetes/master/install_docker_registry_secret.sh
@@ -8,7 +8,7 @@ echo "create  docker-registry secret ..."
 
 kubectl create secret docker-registry registrykey-aliyun -n dev-namespace --docker-server=registry.cn-qingdao.aliyuncs.com --docker-username=313823394@qq.com --docker-password=zx19830113 --docker-email=13823394@qq.com
 
-kubectl create secret docker-registry registrykey-aliyun -n kube-system --docker-server=registry.cn-qingdao.aliyuncs.com --docker-username=13823394@qq.com --docker-password=zx19830113 --docker-email=13823394@qq.com
+kubectl create secret docker-registry registrykey-aliyun-zx -n kube-system --docker-server=registry.cn-qingdao.aliyuncs.com --docker-username=13823394@qq.com --docker-password=zx19830113 --docker-email=13823394@qq.com
 
 echo "install success ..."
 
